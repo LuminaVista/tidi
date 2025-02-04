8969c3ecdbcb6e56731b9e101201d63d0a5de630
Shafayet Ul Islam <shafayet.islam.61@gmail.com>
2025-02-04 12:45:47 +1030

    Initial Commit

---
+++ TIDI/TIDIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIDIApp.swift
+//  TIDI
+//
+//  Created by Shafayet Ul Islam on 4/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct TIDIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
